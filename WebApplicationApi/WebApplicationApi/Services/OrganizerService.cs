d3ae484e32431fdb53ffd801d76917ab94fe0c9c
zarifatb-15 <zarifatb@code.edu.az>
2026-05-28 23:47:53 +0400

    add UploadLogo

--- WebApplicationApi/WebApplicationApi/Services/OrganizerService.cs
+++ WebApplicationApi/WebApplicationApi/Services/OrganizerService.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using WebApplicationApi.Dtos.OrganizerDtos;
 using WebApplicationApi.Entity;
+using WebApplicationApi.Extensions;
 using WebApplicationApi.Repositories;
 
 namespace WebApplicationApi.Services;
@@ -9,10 +10,12 @@ public class OrganizerService:IOrganizerService
 {
     private readonly IRepository<Organizer> _repository;
     private readonly IMapper _mapper;
-    public OrganizerService (IRepository<Organizer> repository, IMapper mapper)
+    private readonly IWebHostEnvironment _env;
+    public OrganizerService (IRepository<Organizer> repository, IMapper mapper, IWebHostEnvironment env)
     {
         _repository = repository;
         _mapper = mapper;
+        _env = env;
     }
     public async Task<List<OrganizerReturnDto>> GetAllAsync()
     {
@@ -32,5 +35,21 @@ public class OrganizerService:IOrganizerService
       var organizer = _mapper.Map<Organizer>(dto);
       await _repository.AddAsync(organizer);
       await _repository.SaveChangesAsync();
+    }
+
+    public async Task UploadLogoAsync(int id, IFormFile file)
+    {
+        var organizer = await _repository.GetByIdAsync(id);
+        if (organizer == null) throw new Exception("Organizer not found");
+        
+        var uploadsFolder = Path.Combine(_env.WebRootPath, "uploads", "organizers");
+        if (!Directory.Exists(uploadsFolder)) Directory.CreateDirectory(uploadsFolder);
+        
+        var savedFileName =await file.SaveFileAsync(uploadsFolder);
+        
+        organizer.LogoUrl = $"/uploads/organizers/{savedFileName}";
+        _repository.Update(organizer);
+        await _repository.SaveChangesAsync();
+        
     }
 }
